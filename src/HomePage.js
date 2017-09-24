476a38c7fe914d81db6aa98ba090148e40540d09
Joey <joey.z.rp@gmail.com>
2017-09-24 16:32:48 +1000

    add typing animation of 3 sections

--- src/HomePage.js
+++ src/HomePage.js
@@ -14,6 +14,31 @@ const HomePage = React.createClass({
         height: PropTypes.number.isRequired
     },
 
+    getInitialState: function(){
+        return {
+          typing1: true,
+          typing2: false,
+          typing3: false,
+        };
+    },
+
+    onAnimationFinish: function(index){
+        switch(index){
+            case 1:
+                this.setState({
+                    typing1: false,
+                    typing2: true
+                });
+                break;
+            case 2:
+                this.setState({
+                    typing2: false,
+                    typing3: true
+                });
+                break;
+        }
+    },
+
     componentDidMount() {
 
     },
@@ -21,10 +46,10 @@ const HomePage = React.createClass({
     render: function(){
         return (
             <div className="home" style={this.props.styleContent} ref={ (div) => this.div = div}>
-                <Typist 
+                { this.state.typing1 ? <Typist 
                     className="typing-animation-1"
                     startDelay={2000} 
-                    avgTypingDelay={150} 
+                    avgTypingDelay={50} 
                     stdTypingDelay={60} 
                     cursor={{
                         show: true,
@@ -36,17 +61,63 @@ const HomePage = React.createClass({
                     delayGenerator={
                         function(mean, std, {line, lineIdx, charIdx, defDelayGenerator}) {
                         if (lineIdx === 0 && charIdx === line.length - 1) {
-                            return 2000;
+                            return 1000;
+                        }
+                        if (lineIdx === 1 && charIdx === line.length - 1) {
+                            return 1000;
+                        }
+                        if (lineIdx === 2 && charIdx === line.length - 1) {
+                            return 1000;
+                        }
+                        if (lineIdx === 3 && charIdx === line.length - 1) {
+                            return 1000;
+                        }
+                        if (lineIdx === 4 && charIdx === line.length - 1) {
+                            return 1000;
                         }
                         return defDelayGenerator();
                         }
-                    } >
+                    } 
+                    onTypingDone={function(){ this.onAnimationFinish(1) }.bind(this)} >
                     Hello there!
                     <br />
                     Welcome to my space.
-                </Typist>
-                <Typist 
+                    <br />
+                    How are you going today?
+                    <br />
+                    Nice weather, isn't it?
+                    <br/>
+                    Alright, alright, done with chitchat.
+                </Typist> : null }
+
+                { this.state.typing2 ? <Typist 
                     className="typing-animation-2"
+                    startDelay={1000} 
+                    avgTypingDelay={50} 
+                    stdTypingDelay={60} 
+                    cursor={{
+                        show: true,
+                        blink: true,
+                        element: '▌',
+                        hideWhenDone: false,
+                        hideWhenDoneDelay: 1000,
+                    }} 
+                    delayGenerator={
+                        function(mean, std, {line, lineIdx, charIdx, defDelayGenerator}) {
+                        if (lineIdx === 0 && charIdx === line.length - 1) {
+                            return 1000;
+                        }
+                        return defDelayGenerator();
+                        }
+                    } 
+                    onTypingDone={function(){ this.onAnimationFinish(2) }.bind(this)} >
+                    My name is Joey,
+                    <br />
+                    an enthusiastic web developer.
+                </Typist> : null }
+
+                { this.state.typing3 ? <Typist 
+                    className="typing-animation-3"
                     startDelay={1000} 
                     avgTypingDelay={150} 
                     stdTypingDelay={60} 
@@ -65,10 +136,9 @@ const HomePage = React.createClass({
                         return defDelayGenerator();
                         }
                     } >
-                    My name is Joey,
-                    <br />
-                    an enthusiastic web developer.
-                </Typist>
+                    Would you like to know more about me?
+                </Typist> : null }
+
                 <HomePageBackground width={this.props.width} height={this.props.height}/>
                 <button onClick={this.props.handleTurn}>Turn</button>
             </div>
