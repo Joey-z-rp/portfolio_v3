f459e7e52579ee04eef37be80d46f47edb1fd17c
Joey <joey.z.rp@gmail.com>
2017-09-25 10:34:55 +1000

    add nav button style

--- src/NavMenu.js
+++ src/NavMenu.js
@@ -5,17 +5,17 @@ const NavMenu = props => {
     return (
         <div className="nav">
             { props.currentPage === "about" ? null : <button 
-                className="btn-about" 
+                className="btn-about btn" 
                 onClick={() => {props.handleTurn(props.currentPage, "about")}}>
                 About Me
             </button> }
             { props.currentPage === "skill" ? null : <button 
-                className="btn-skill" 
+                className="btn-skill btn" 
                 onClick={() => {props.handleTurn(props.currentPage, "skill")}}>
                 Skillset
             </button> }
             { props.currentPage === "link" ? null : <button 
-                className="btn-link" 
+                className="btn-link btn" 
                 onClick={() => {props.handleTurn(props.currentPage, "link")}}>
                 Contact
             </button> }
